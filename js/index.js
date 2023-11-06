6e99a55abdc804fd659dbd2c96080ec6f91b5cf9
TeraBorn <150022539+TeraBorn@users.noreply.github.com>
2023-11-06 08:30:13 -0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,27 @@
+// Select DOM elements
+const navbarLink = document.querySelector('#navbar-link');
+const contactForm = document.querySelector('#contact-form');
+const emailInput = document.querySelector('#emailInput');
+const subjectInput = document.querySelector('#subjectInput');
+const messageInput = document.querySelector('#messageInput');
+
+// Regular expression to validate e-mails
+const regex = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
+
+// Functions that check the validity of text fields
+const isValidEmail = () => regex.test(emailInput.value);
+const isValidSubject = () => subjectInput.value.trim() !== '';
+const isValidMessage = () => messageInput.value.trim() !== '';
+
+const isValidInputs = () => isValidEmail() && isValidSubject() && isValidMessage();
+
+const DOMLoaded = () => {
+    navbarLink.addEventListener('click', () => window.location.reload());
+    contactForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+        if(isValidInputs())
+            contactForm.submit();
+    });
+};
+
+document.addEventListener('DOMContentLoaded', DOMLoaded)
